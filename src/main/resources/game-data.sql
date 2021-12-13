6542ddbd63020dc4be331255063116d4e30413db
SarahDowning <sarahdowningx@gmail.com>
2021-12-13 10:52:02 +0000

    GLA-24 #comment Added resources files with schema, app properties etc.

---
+++ src/main/resources/game-data.sql
@@ -0,0 +1,15 @@
+INSERT INTO `game` (
+	`id`,
+	`game_Title`,
+	`genre`,
+	`publisher`,
+	`platform`,
+	`release_Year`
+) values (
+	1,
+	'Final Fantasy I',
+	'RPG',
+	'Square Enix',
+	'Steam',
+	2021
+);
